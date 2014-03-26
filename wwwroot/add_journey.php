c196572feafd8917db861e26f74ada4134e863aa
Tommy <tommy@Tommys-MacBook-Pro-3.local>
2014-03-26 14:28:02 +0000

    fixing bug with the stupid button

--- wwwroot/add_journey.php
+++ wwwroot/add_journey.php
@@ -5,7 +5,7 @@ if(array_key_exists("home", $_REQUEST))
   print('<hr />');
 }
 ?>
-<form method="post" action="add_journey.php">
+<form method="post" action="index.php">
 <label for="home">Home: <input id="home" name="home" type="text"/></label>
 <input type="submit" />
 </form>
